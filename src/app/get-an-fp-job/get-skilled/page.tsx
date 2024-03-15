589edbdaaca5b6a9e018bdd900134d2ae41be3e7
Stefan Wullems <stefanwullems@Stefans-MacBook-Air.local>
2024-03-15 12:45:25 +0100

    better header

--- src/app/get-an-fp-job/get-skilled/page.tsx
+++ src/app/get-an-fp-job/get-skilled/page.tsx
@@ -14,7 +14,8 @@ export default function HeroLanding() {
     <Container className="mt-9">
       <div className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-          45 Days {`"Get an FP Job"`} Program
+          45 Days <span className="text-emerald-500">Get an FP Job</span>{' '}
+          Program
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
           You might not always need the experience you think you need. I was
