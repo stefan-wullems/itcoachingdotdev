0182ea3dc9ad453595b19f6d46bcf76cafe24f35
Stefan Wullems <stefanwullems@stefans-air.home>
2024-04-01 11:12:18 +0200

    hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
 
 import '@/styles/tailwind.css'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Tech Career Coach - Stefan Wullems',
@@ -43,6 +44,19 @@ export default function RootLayout({
           <div className="w-full">{children}</div>
         </Providers>
       </body>
+      <Script id="hotjar">
+        {`<!-- Hotjar Tracking Code for https://www.itcoaching.dev -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3927183,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>`}
+      </Script>
     </html>
   )
 }
